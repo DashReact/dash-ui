b3aa0c8391507043d8201438eb58bda820d10acc
Pablo Aballe <pablo970616@gmail.com>
2023-01-01 18:57:26 +0100

    [ FEAT ] Change theme feature

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,1 +1,15 @@
-import './styles.css';
+
+
+export const getTheme = () => {
+    // get data-theme attribute
+    const theme = document.body.getAttribute('data-theme');
+    return theme || 'light';
+}
+
+export const changeTheme = (newTheme: string) => {
+    // Change data-theme attribute 
+    document.body.setAttribute('data-theme', newTheme);
+    return newTheme;
+}
+
+
--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,83 +1,110 @@
 import React from 'react';
+import { changeTheme, getTheme } from '@dash-ui-lib/styles';
 
 export function Index() {
   return (
-    <div className={'m-md p-sm shadow-lg round-md'}>
-      <div className="pb-sm">Buttons</div> <br />
-      <div className={`flex items-center justify-between gap-sm flex-wrap`}>
-        <button className="btn btn-primary">prueba</button>
-        <button className="btn btn-secondary">test</button>
-        <button className="btn btn-white">test</button>
-        <button className="btn btn-black">test</button>
-        <button className="btn btn-gray">test</button>
-        <button className="btn btn-red">test</button>
-        <button className="btn btn-orange">test</button>
-        <button className="btn btn-yellow">test</button>
-        <button className="btn btn-green">test</button>
-        <button className="btn btn-teal">test</button>
-        <button className="btn btn-blue">test</button>
-        <button className="btn btn-indigo">test</button>
-        <button className="btn btn-cyan">test</button>
-        <button className="btn btn-purple">test</button>
-        <button className="btn btn-pink">test</button>
-        <button className="btn btn-sm">test</button>
-        <button className="btn btn-md">test</button>
-        <button className="btn btn-lg">test</button>
-        <button className="btn btn-xl">test</button>
-        <button className="btn btn-full">test</button>
-        <button className="btn btn-rounded">test</button>
-        <button className="btn btn-rounded-sm">test</button>
-        <button className="btn btn-rounded-md">test</button>
-        <button className="btn btn-rounded-lg">test</button>
-        <button className="btn btn-rounded-full">test</button>
-        <button className="btn btn-outline">test</button>
-        <button className="btn btn-outline-primary">test</button>
-        <button className="btn btn-outline-secondary">test</button>
-        <button className="btn btn-outline-white">test</button>
-        <button className="btn btn-outline-black">test</button>
-        <button className="btn btn-outline-gray">test</button>
-        <button className="btn btn-outline-red">test</button>
-        <button className="btn btn-outline-orange">test</button>
-        <button className="btn btn-outline-yellow">test</button>
-        <button className="btn btn-outline-green">test</button>
-        <button className="btn btn-outline-teal">test</button>
-        <button className="btn btn-outline-blue">test</button>
-        <button className="btn btn-outline-indigo">test</button>
-        <button className="btn btn-outline-cyan">test</button>
-        <button className="btn btn-outline-purple">test</button>
-        <button className="btn btn-outline-pink">test</button>
-        <button className="btn btn-ghost">test</button>
-        <button className="btn btn-ghost-primary">test</button>
-        <button className="btn btn-ghost-secondary">test</button>
-        <button className="btn btn-ghost-white">test</button>
-        <button className="btn btn-ghost-black">test</button>
-        <button className="btn btn-ghost-gray">test</button>
-        <button className="btn btn-ghost-red">test</button>
-        <button className="btn btn-ghost-orange">test</button>
-        <button className="btn btn-ghost-yellow">test</button>
-        <button className="btn btn-ghost-green">test</button>
-        <button className="btn btn-ghost-teal">test</button>
-        <button className="btn btn-ghost-blue">test</button>
-        <button className="btn btn-ghost-indigo">test</button>
-        <button className="btn btn-ghost-cyan">test</button>
-        <button className="btn btn-ghost-purple">test</button>
-        <button className="btn btn-ghost-pink">test</button>
-        <button className="btn btn-link">test</button>
-        <button className="btn btn-link-primary">test</button>
-        <button className="btn btn-link-secondary">test</button>
-        <button className="btn btn-link-white">test</button>
-        <button className="btn btn-link-black">test</button>
-        <button className="btn btn-link-gray">test</button>
-        <button className="btn btn-link-red">test</button>
-        <button className="btn btn-link-orange">test</button>
-        <button className="btn btn-link-yellow">test</button>
-        <button className="btn btn-link-green">test</button>
-        <button className="btn btn-link-teal">test</button>
-        <button className="btn btn-link-blue">test</button>
-        <button className="btn btn-link-indigo">test</button>
-        <button className="btn btn-link-cyan">test</button>
-        <button className="btn btn-link-purple">test</button>
-        <button className="btn btn-link-pink">test</button>
+    <div>
+      <div className={'m-md p-sm shadow-lg round-md'}>
+        <div className="pb-sm text-bold">Buttons</div> <br />
+        <div className={`flex items-center justify-between gap-sm flex-wrap`}>
+          <button className="btn btn-primary">prueba</button>
+          <button className="btn btn-secondary">test</button>
+          <button className="btn btn-white">test</button>
+          <button className="btn btn-black">test</button>
+          <button className="btn btn-gray">test</button>
+          <button className="btn btn-red">test</button>
+          <button className="btn btn-orange">test</button>
+          <button className="btn btn-yellow">test</button>
+          <button className="btn btn-green">test</button>
+          <button className="btn btn-teal">test</button>
+          <button className="btn btn-blue">test</button>
+          <button className="btn btn-indigo">test</button>
+          <button className="btn btn-cyan">test</button>
+          <button className="btn btn-purple">test</button>
+          <button className="btn btn-pink">test</button>
+          <button className="btn btn-sm">test</button>
+          <button className="btn btn-md">test</button>
+          <button className="btn btn-lg">test</button>
+          <button className="btn btn-xl">test</button>
+          <button className="btn btn-full">test</button>
+          <button className="btn btn-rounded">test</button>
+          <button className="btn btn-rounded-sm">test</button>
+          <button className="btn btn-rounded-md">test</button>
+          <button className="btn btn-rounded-lg">test</button>
+          <button className="btn btn-rounded-full">test</button>
+          <button className="btn btn-outline">test</button>
+          <button className="btn btn-outline-primary">test</button>
+          <button className="btn btn-outline-secondary">test</button>
+          <button className="btn btn-outline-white">test</button>
+          <button className="btn btn-outline-black">test</button>
+          <button className="btn btn-outline-gray">test</button>
+          <button className="btn btn-outline-red">test</button>
+          <button className="btn btn-outline-orange">test</button>
+          <button className="btn btn-outline-yellow">test</button>
+          <button className="btn btn-outline-green">test</button>
+          <button className="btn btn-outline-teal">test</button>
+          <button className="btn btn-outline-blue">test</button>
+          <button className="btn btn-outline-indigo">test</button>
+          <button className="btn btn-outline-cyan">test</button>
+          <button className="btn btn-outline-purple">test</button>
+          <button className="btn btn-outline-pink">test</button>
+          <button className="btn btn-ghost">test</button>
+          <button className="btn btn-ghost-primary">test</button>
+          <button className="btn btn-ghost-secondary">test</button>
+          <button className="btn btn-ghost-white">test</button>
+          <button className="btn btn-ghost-black">test</button>
+          <button className="btn btn-ghost-gray">test</button>
+          <button className="btn btn-ghost-red">test</button>
+          <button className="btn btn-ghost-orange">test</button>
+          <button className="btn btn-ghost-yellow">test</button>
+          <button className="btn btn-ghost-green">test</button>
+          <button className="btn btn-ghost-teal">test</button>
+          <button className="btn btn-ghost-blue">test</button>
+          <button className="btn btn-ghost-indigo">test</button>
+          <button className="btn btn-ghost-cyan">test</button>
+          <button className="btn btn-ghost-purple">test</button>
+          <button className="btn btn-ghost-pink">test</button>
+          <button className="btn btn-link">test</button>
+          <button className="btn btn-link-primary">test</button>
+          <button className="btn btn-link-secondary">test</button>
+          <button className="btn btn-link-white">test</button>
+          <button className="btn btn-link-black">test</button>
+          <button className="btn btn-link-gray">test</button>
+          <button className="btn btn-link-red">test</button>
+          <button className="btn btn-link-orange">test</button>
+          <button className="btn btn-link-yellow">test</button>
+          <button className="btn btn-link-green">test</button>
+          <button className="btn btn-link-teal">test</button>
+          <button className="btn btn-link-blue">test</button>
+          <button className="btn btn-link-indigo">test</button>
+          <button className="btn btn-link-cyan">test</button>
+          <button className="btn btn-link-purple">test</button>
+          <button className="btn btn-link-pink">test</button>
+          <button className="btn btn-blue">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              className='bg-none'
+            >
+              <path d="M9.752 6.193c.599.6 1.73.437 2.528-.362.798-.799.96-1.932.362-2.531-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532Z" />
+              <path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9.42 9.42 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a9.556 9.556 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093.067.017.12.033.16.045.184.06.279.13.351.295l.029.073a3.475 3.475 0 0 1 .157.721c.055.485.051 1.178-.159 2.065Zm-4.828 7.475.04-.04-.107 1.081a1.536 1.536 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a8.548 8.548 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006ZM5.205 5c-.625.626-.94 1.351-1.004 2.09a8.497 8.497 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107-.04.039Zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a2.835 2.835 0 0 0-.045-.283 3.078 3.078 0 0 0-.3-.041Z" />
+              <path d="M7.009 12.139a7.632 7.632 0 0 1-1.804-1.352A7.568 7.568 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z" />
+            </svg>
+            test
+          </button>
+        </div>
+      </div>
+      <div className={'m-md p-sm shadow-lg round-md'}>
+        <div className="pb-sm text-bold">Change theme</div> <br />
+        <button
+          className="btn btn-primary"
+          onClick={() => changeTheme(getTheme() === 'dark' ? 'light' : 'dark')}
+        >
+          Toggle theme
+        </button>
       </div>
     </div>
   );
